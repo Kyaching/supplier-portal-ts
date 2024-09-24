9ad85530091940683da46b3c784e46f742a997bf
kyaching <kyochingmarma@gmail.com>
2024-09-24 14:07:19 +0600

    created departments route api

---
+++ server/src/types.ts
@@ -0,0 +1,20 @@
+export interface CreateUserData {
+  username: string;
+  first_name: string;
+  middle_name?: string;
+  last_name: string;
+  email: string;
+  password: string;
+  job_title_id: string;
+  job_title: string;
+  user_type_id: string;
+  user_type: string;
+}
+
+export interface UserData {
+  id: string;
+  first_name: string;
+  last_name: string;
+  job_title: string;
+  job_title_id: string;
+}
